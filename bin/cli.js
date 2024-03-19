1f204532505d297710c066c232d6343a77b8f68a
[lahiru7239] <[Your>
2024-03-19 13:56:21 +0530

    feat:npm package making

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,24 @@
-const { exeSync } = require("child_process");
+#!/usr/bin/env node
+const { execSync } = require("child_process");
 
 const runCommand = (command) => {
   try {
-    exeSync(`${command}`, { stdio: "inherit" });
+    execSync(`${command}`, { stdio: "inherit" });
   } catch (e) {
+    console.error(`failed to execute ${command}`, e);
     return false;
   }
   return true;
 };
 
-const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/lahiruExpernetic/testbolt-automation.git ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const repoName = "testbolt-automation";
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/lahiruExpernetic/testbolt-automation.git`;
+// const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`cloning repo with ${repoName}`);
 
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
+// const installedDeps = runCommand(installDepsCommand);
+// if (!installedDeps) process.exit(-1);
